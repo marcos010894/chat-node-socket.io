6357934eff5d65cbaa7421b7c692039406388bbe
Marcos Machado <marcospaulo.dev@hotmail.com>
2021-10-29 13:36:39 -0300

    iniciando o projeto

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { connect } = require('http2');
+const app = express();
+const http = require('http').createServer(app);
+const PORT = process.env.PORT || 5000;
+const io = require('socket.io')(http, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+})
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html' )
+})
+
+io.on('connection', (socket) => {
+    console.log(`new console ${socket.id}`)
+    socket.on('distribution', (msg) =>{       
+        console.log(msg)
+        io.emit('distribution', msg)
+    })
+})
+
+http.listen(PORT, () => {
+    console.log('listemPort', PORT)
+})
