2a2838d07ace8a28d202645e912198f248544de8
Marcos Paulo Machado Azevedo <marcospaulo.dev@hotmail.com>
2023-09-27 22:52:35 -0300

    alterando css e link o socketIO

--- server.js
+++ server.js
@@ -1,44 +1,42 @@
-const express = require('express');
-const { connect } = require('http2');
+const express = require("express");
+const { connect } = require("http2");
 const app = express();
-const http = require('http').createServer(app);
+const http = require("http").createServer(app);
 const PORT = process.env.PORT || 5000;
-const io = require('socket.io')(http, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Methods": "GET,POST",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": "*", //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true,
-            
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-})
+const io = require("socket.io")(http, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      "Access-Control-Allow-Methods": "GET,POST",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": "*", //or the specific origin you want to give access to,
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
 
-app.use('/ckeditor', express.static('ckeditor'))
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html' )
-})
+app.use("/ckeditor", express.static("ckeditor"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-io.on('connection', (socket) => {
-    console.log(`new console ${socket.id}`)
-    socket.on('distribution', (msg) =>{       
-        console.log(msg)
-        io.emit('distribution', msg)
-    })
-})
+io.on("connection", (socket) => {
+  console.log(`new console ${socket.id}`);
+  socket.on("distribution", (msg) => {
+    console.log(msg);
+    io.emit("distribution", msg);
+  });
+});
 
 http.listen(PORT, () => {
-    console.log('listemPort', PORT)
-})
+  console.log("listemPort", PORT);
+});
 
-app.get('/style', (req, res) => {
-    res.sendFile(__dirname+ '/css/styled.css')
-})
+app.get("/style", (req, res) => {
+  res.sendFile(__dirname + "/css/styled.css");
+});
 
-app.get('/js', (req, res) => {
-    res.sendFile(__dirname+  '/js/mains.js')
-}) 
-
+app.get("/js", (req, res) => {
+  res.sendFile(__dirname + "/js/mains.js");
+});
